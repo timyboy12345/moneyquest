23fc55640843e8aee07e3bb195fac1d2eee9e860
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-03-27 17:10:19 +0100

    login scherm

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layout/master')
+
+@section('title', "MoneyQ - Home")
+
+@section('content')
+    @if (Route::has('login'))
+        <div class="flex-center position-ref full-height">
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        </div>
+    @endif
+
+    <a href="/" class="title">MoneyQ</a>
+    <div class="wrapper">
+        <div class="block">
+            <h3 class="text-center">Inloggen</h3>
+            <form>
+                <div class="formline">
+                    <label for="email">Email</label>
+                    <input type="email" placeholder="Email" id="email">
+                </div>
+                <div class="formline">
+                    <label for="email">Wachtwoord</label>
+                    <input type="password" placeholder="Wachtwoord" id="password">
+                </div>
+                <input class="button" type="submit">
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
+use Mollie\Api\MollieApiClient;
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', function() {
+    return view('login/index');
+})->name('login');
+
+Route::get('/mollie', function () {
+    $payment_id = "12345";
+
+    $mollie = new MollieApiClient();
+    $mollie->setApiKey("test_dGjvGh2bqaVStDgJDGbeqnbqj2qJzm");
+
+    try {
+        $payment = $mollie->payments->create([
+            "amount" => [
+                "currency" => "EUR",
+                "value" => "10.00"
+            ],
+            "description" => "My first API payment",
+            "redirectUrl" => "http://127.0.0.1:8000/return/" . $payment_id,
+            "webhookUrl" => "http://127.0.0.1:8000/webhook/"
+        ]);
+
+//    $payment->getCheckoutUrl();
+        return redirect($payment->getCheckoutUrl());
+    } catch (Exception $e) {
+        info($e);
+        return view('mollie/error', ["error" => $e]);
+    }
 });

014f00e87cb36633df1f50701ed6db5334bedbf5
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-05-31 14:59:57 +0200

    Font awsome bullshit

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,7 +12,7 @@
     <link href="/css/account.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
 </head>
 <body>
 @yield('content')
